9e4b1e82115def49f7b4a4b0b827ff83e8109c74
Minghan Xu <mxu57@myseneca.ca>
2018-09-14 02:17:11 -0400

    finish

--- public/js/pokemoncatch.js
+++ public/js/pokemoncatch.js
@@ -3,9 +3,11 @@ var speed;
 var score;
 var max = 5000;
 var step = 100;
+var random;
+var picURL;
 function renewBUG() {
 		var bug = document.getElementById('pokemon');
-		random = Math.floor(Math.random() * 5) + 1;
+		random = Math.floor(Math.random() * 500) + 1;
 		picURL = "url(../images/" + random + ".svg)";
 		randX = Math.floor(Math.random() * 736) + 1;
 		randY = Math.floor(Math.random() * 436) + 1;
@@ -18,6 +20,7 @@ function goal() {
 	document.getElementById('score').value = score;
 	speed = speed + step;
 	document.getElementById('speed').value = speed;
+	updateInfo();
 	renewBUG();
 	clearInterval(autoAdvance);
 	autoAdvance = setInterval("renewBUG()", max - speed);
@@ -39,4 +42,41 @@ function init() {
 	score = 0;
 	document.getElementById('score').value = 0;
 	document.getElementById('speed').value = 0;
+}
+
+function updateInfo() {
+	let httpRequest = new XMLHttpRequest();
+	httpRequest.open("POST", "/getInfo");
+	httpRequest.setRequestHeader("Content-Type", "application/json");
+	httpRequest.send(JSON.stringify({pid: random}));
+
+	httpRequest.onreadystatechange = () => {
+		if (httpRequest.readyState === 4) {
+			if (httpRequest.status === 200) {
+				res = JSON.parse(httpRequest.responseText);
+				picURL = "url(../images/" + res[0].id + ".svg)";
+				document.getElementById('pimg').style.backgroundImage = picURL;
+				document.getElementById('pname').innerText = res[0].name;
+
+				document.getElementById('php').innerText = "HP " + res[0].hp;
+				document.getElementById('php').style.backgroundColor = "green";
+				document.getElementById('php').style.width = 80 + res[0].hp;
+
+				document.getElementById('pattack').innerText = "ATTACK " + res[0].attack;
+				document.getElementById('pattack').style.backgroundColor = "red";
+				document.getElementById('pattack').style.width = 80 + res[0].attack;
+
+				document.getElementById('pdefense').innerText = "DEFENSE " + res[0].denfense;
+				document.getElementById('pdefense').style.backgroundColor = "orange";
+				document.getElementById('pdefense').style.width = 80 + res[0].denfense;
+
+				document.getElementById('pspeed').innerText = "SPEED " + res[0].speed;
+				document.getElementById('pspeed').style.backgroundColor = "#81a7d6";
+				document.getElementById('pspeed').style.width = 80 + res[0].speed;
+
+			}else{
+				console.log("Error getting Data");
+			}
+		}
+	}
 }
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/getInfo", bodyParser.json(), (req, res) => {
+    var mongoose = require("mongoose");
+
+    var url = "mongodb://admin:passw0rd@ds255332.mlab.com:55332/pokemon_db";
+    
+    var conn = mongoose.createConnection(url);
+    conn.once('open', ()=>{
+        conn.collection('pekemon_sch').find({id: req.body.pid}).toArray(function(err, data){
+            res.json(data);
+        }); 
+    });
 });
 
 app.listen(HTTP_PORT);
